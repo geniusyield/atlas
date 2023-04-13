3e597535db963fddbe5d5a32f48206d2e8307dab
sourabhxyz <swasti@sourabh.xyz>
2023-04-13 11:15:20 +0530

    feat 95: multi asset collateral support - run only normal test-suite in CI

--- tests-privnet/atlas-privnet-tests.hs
+++ tests-privnet/atlas-privnet-tests.hs
@@ -1,3 +1,11 @@
+{-|
+Module      : Main
+Copyright   : (c) 2023 GYELD GMBH
+License     : Apache 2.0
+Maintainer  : support@geniusyield.co
+Stability   : develop
+
+-}
 module Main (main) where
 
 import           GeniusYield.Imports

a6f03a8b92c99916d849b57270920d6a33c30448
Valentino Cerutti <valentinocerutti@gmail.com>
2023-08-30 17:19:53 -0300

    Feat #166 & #167: Updates nix config

--- nix/hix.nix
+++ nix/hix.nix
@@ -1,6 +1,6 @@
 {pkgs, config, ...}: {
   # name = "project-name";
-  compiler-nix-name = "ghc8107"; # Version of GHC to use
+  compiler-nix-name = "ghc928"; # Version of GHC to use
 
   # Cross compilation support:
   # crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
@@ -13,15 +13,13 @@
   # Tools to include in the development shell
   shell = {
     tools = {
-      cabal = "3.8.1.0";
-      hlint = "3.4.1";
-      haskell-language-server = { version = "1.8.0.0"; index-state = "2022-12-17T00:00:00Z"; };
+      cabal = "3.10.1.0";
+      hlint = "3.6.1";
+      haskell-language-server = { version = "2.0.0.0"; index-state = "2023-06-05T06:39:32Z"; };
     };
 
     buildInputs = with pkgs; [
       jq
-      config.hsPkgs.cardano-node.components.exes.cardano-node
-      config.hsPkgs.cardano-cli.components.exes.cardano-cli
     ];
   };
 }

3cd87be171b2e5e9b26ed9c818cff41ede9811d1
ajuggler <ajuggler@users.noreply.github.com>
2024-02-13 10:13:25 -0600

    refined isomorphism between GYTxMetadata and Api.TxMetadata

--- src/GeniusYield/TxBuilder/Class.hs
+++ src/GeniusYield/TxBuilder/Class.hs
@@ -371,7 +371,7 @@ instance Semigroup (GYTxSkeleton v) where
         , gytxSigs          = Set.union (gytxSigs x) (gytxSigs y)
         , gytxInvalidBefore = combineInvalidBefore (gytxInvalidBefore x) (gytxInvalidBefore y)
         , gytxInvalidAfter  = combineInvalidAfter (gytxInvalidAfter x) (gytxInvalidAfter y)
-        , gytxMetadata      = combineMetadata (gytxMetadata x) (gytxMetadata y)
+        , gytxMetadata      = (Just combineMetadata) <*> (gytxMetadata x) <*> (gytxMetadata y)
         }
       where
         -- we keep only one input per utxo to spend
@@ -390,9 +390,8 @@ instance Semigroup (GYTxSkeleton v) where
         combineInvalidAfter (Just s) (Just t) = Just (min s t)
 
         -- Keep the last metadata assigned in a skeleton
-        combineMetadata :: Maybe GYTxMetadata -> Maybe GYTxMetadata -> Maybe GYTxMetadata
-        combineMetadata mx Nothing = mx
-        combineMetadata _  my      = my
+        combineMetadata :: GYTxMetadata -> GYTxMetadata -> GYTxMetadata
+        combineMetadata mdx mdy = metadataFromApi $ (metadataToApi mdx) <> (metadataToApi mdy)
 
 instance Monoid (GYTxSkeleton v) where
     mempty = emptyGYTxSkeleton
--- src/GeniusYield/Types/TxMetadata.hs
+++ src/GeniusYield/Types/TxMetadata.hs
@@ -1,27 +1,71 @@
 module GeniusYield.Types.TxMetadata (
   GYTxMetadata (..),
+  GYTxMetadataValue,
+  gyTxMetaMap,
+  gyTxMetaList,
+  gyTxMetaNumber,
+  gyTxMetaBytes,
+  gyTxMetaText,
+  makeApiTransactionMetadataValue,
   metadataFromApi,
-  metadataMsg
+  metadataToApi,
+  metadataMsg,
+  metadataMsgs
 ) where
 
 import qualified Cardano.Api as Api
 import qualified Data.Map as Map
-import           Data.Text (Text)
+import           Data.ByteString     (ByteString)
+import           Data.Word           (Word64)
+import           GeniusYield.Imports (Text, coerce)
 
 
 newtype GYTxMetadata = GYTxMetadata (Api.TxMetadataInEra Api.BabbageEra)
   deriving Show
 
+type GYTxMetadataValue = Api.TxMetadataValue
+
+gyTxMetaMap :: [(GYTxMetadataValue, GYTxMetadataValue)] -> GYTxMetadataValue
+gyTxMetaMap = Api.TxMetaMap
+
+gyTxMetaList :: [GYTxMetadataValue] -> GYTxMetadataValue
+gyTxMetaList = Api.TxMetaList
+
+gyTxMetaNumber :: Integer -> GYTxMetadataValue
+gyTxMetaNumber = Api.TxMetaNumber
+
+gyTxMetaBytes :: ByteString -> GYTxMetadataValue
+gyTxMetaBytes = Api.TxMetaBytes
+
+gyTxMetaText :: Text -> GYTxMetadataValue
+gyTxMetaText = Api.TxMetaText
+
+makeApiTransactionMetadataValue :: Map.Map Word64 GYTxMetadataValue -> Api.TxMetadata
+makeApiTransactionMetadataValue = Api.TxMetadata
+
+
+
 metadataFromApi :: Api.TxMetadata -> GYTxMetadata
 metadataFromApi md@(Api.TxMetadata kvm)
   | Map.null kvm = GYTxMetadata Api.TxMetadataNone
-  | otherwise    = GYTxMetadata . Api.TxMetadataInEra Api.TxMetadataInBabbageEra $ md
+  | otherwise    = GYTxMetadata (Api.TxMetadataInEra Api.TxMetadataInBabbageEra md)
 
-metadataMsg :: Text -> GYTxMetadata
-metadataMsg msg = metadataFromApi . Api.makeTransactionMetadata $
-                  Map.fromList [(674, Api.TxMetaMap [(Api.TxMetaText "msg", Api.TxMetaList [Api.TxMetaText msg])])]
+metadataToApi :: GYTxMetadata -> Api.TxMetadata
+metadataToApi gymd = case coerce gymd of
+  Api.TxMetadataNone -> mempty
+  Api.TxMetadataInEra Api.TxMetadataInBabbageEra md -> md
 
 
--- TODO  other direction :  metadataToApi  (coerce)
 
--- Keep Maybe in skeleton
+-- | Adds a message (comment/memo) as transaction metadata following CIP 020 specification.
+metadataMsg :: Text -> Maybe GYTxMetadata
+metadataMsg msg = metadataMsgs [msg]
+
+-- | Adds messages (comments/memos) as transaction metadata following CIP 020 specification.
+metadataMsgs :: [Text] -> Maybe GYTxMetadata
+metadataMsgs msgs = case repleteMsgs of
+  [] -> Nothing
+  _  -> Just . metadataFromApi . makeApiTransactionMetadataValue $
+        Map.fromList [(674, gyTxMetaMap [(gyTxMetaText "msg", gyTxMetaList $ gyTxMetaText <$> repleteMsgs)])]
+  where
+    repleteMsgs = filter (/= "") msgs

190267227994582c57ddd90c71a569a32c7ef14c
ajuggler <ajuggler@users.noreply.github.com>
2024-02-13 22:01:50 -0600

    Can deal with long messages using 'chunks'

--- src/GeniusYield/Types/TxMetadata.hs
+++ src/GeniusYield/Types/TxMetadata.hs
@@ -6,7 +6,10 @@ module GeniusYield.Types.TxMetadata (
   gyTxMetaNumber,
   gyTxMetaBytes,
   gyTxMetaText,
-  makeApiTransactionMetadataValue,
+  gyMetaTextChunks,
+  gyMetaBytesChunks,
+  mergeGYTransactionMetadata,
+  makeApiTransactionMetadata,
   metadataFromApi,
   metadataToApi,
   metadataMsg,
@@ -20,52 +23,89 @@ import           Data.Word           (Word64)
 import           GeniusYield.Imports (Text, coerce)
 
 
+
 newtype GYTxMetadata = GYTxMetadata (Api.TxMetadataInEra Api.BabbageEra)
   deriving Show
 
 type GYTxMetadataValue = Api.TxMetadataValue
 
-gyTxMetaMap :: [(GYTxMetadataValue, GYTxMetadataValue)] -> GYTxMetadataValue
-gyTxMetaMap = Api.TxMetaMap
+--------------------------------------------------------------------------------
+-- Isomorphism between 'Cardano.Api.TxMetadata' and 'GYTxMetadata'
+--------------------------------------------------------------------------------
 
-gyTxMetaList :: [GYTxMetadataValue] -> GYTxMetadataValue
-gyTxMetaList = Api.TxMetaList
-
-gyTxMetaNumber :: Integer -> GYTxMetadataValue
-gyTxMetaNumber = Api.TxMetaNumber
-
-gyTxMetaBytes :: ByteString -> GYTxMetadataValue
-gyTxMetaBytes = Api.TxMetaBytes
-
-gyTxMetaText :: Text -> GYTxMetadataValue
-gyTxMetaText = Api.TxMetaText
-
-makeApiTransactionMetadataValue :: Map.Map Word64 GYTxMetadataValue -> Api.TxMetadata
-makeApiTransactionMetadataValue = Api.TxMetadata
-
-
-
+-- | Convert 'Cardano.Api.TxMetadata' to 'GYTxMetadata'.
 metadataFromApi :: Api.TxMetadata -> GYTxMetadata
 metadataFromApi md@(Api.TxMetadata kvm)
   | Map.null kvm = GYTxMetadata Api.TxMetadataNone
   | otherwise    = GYTxMetadata (Api.TxMetadataInEra Api.TxMetadataInBabbageEra md)
 
+-- | Convert 'GYTxMetadata' to 'Cardano.Api.TxMetadata'.
 metadataToApi :: GYTxMetadata -> Api.TxMetadata
 metadataToApi gymd = case coerce gymd of
   Api.TxMetadataNone -> mempty
   Api.TxMetadataInEra Api.TxMetadataInBabbageEra md -> md
 
 
+--------------------------------------------------------------------------------
+-- Convenience functions for dealing with 'GYTxMetadataValue's and 'TxMetadata'
+--------------------------------------------------------------------------------
 
--- | Adds a message (comment/memo) as transaction metadata following CIP 020 specification.
+-- | Apply 'Map' constructor of 'GYTxMetadataValue'.
+gyTxMetaMap :: [(GYTxMetadataValue, GYTxMetadataValue)] -> GYTxMetadataValue
+gyTxMetaMap = Api.TxMetaMap
+
+-- | Apply 'List' constructor of 'GYTxMetadataValue'.
+gyTxMetaList :: [GYTxMetadataValue] -> GYTxMetadataValue
+gyTxMetaList = Api.TxMetaList
+
+-- | Apply 'Number' constructor of 'GYTxMetadataValue'.
+gyTxMetaNumber :: Integer -> GYTxMetadataValue
+gyTxMetaNumber = Api.TxMetaNumber
+
+-- | Apply 'Bytes' constructor of 'GYTxMetadataValue'.
+gyTxMetaBytes :: ByteString -> GYTxMetadataValue
+gyTxMetaBytes = Api.TxMetaBytes
+
+-- | Apply 'Text' constructor of 'GYTxMetadataValue'.
+gyTxMetaText :: Text -> GYTxMetadataValue
+gyTxMetaText = Api.TxMetaText
+
+-- | Create a 'GYTxMetadataValue' from a Text as a list of chunks of an acceptable size.
+gyMetaTextChunks :: Text -> GYTxMetadataValue
+gyMetaTextChunks = Api.metaTextChunks
+
+-- | Create a 'GYTxMetadataValue' from a 'ByteString' as a list of chunks of an accaptable size.
+gyMetaBytesChunks :: ByteString -> GYTxMetadataValue
+gyMetaBytesChunks = Api.metaBytesChunks
+
+-- | Merge two 'GYTxMetadata's, controlling how to handle the respective 'GYTxMetadataValue's in case of label collision.
+mergeGYTransactionMetadata :: (GYTxMetadataValue -> GYTxMetadataValue -> GYTxMetadataValue) -> GYTxMetadata -> GYTxMetadata -> GYTxMetadata
+mergeGYTransactionMetadata f gymd1 gymd2 = metadataFromApi $ Api.mergeTransactionMetadata f (metadataToApi gymd1) (metadataToApi gymd2)
+
+-- | Apply 'TxMetadata' wrapper to a 'Map Word64 GYTxMetadataValue'.
+makeApiTransactionMetadata :: Map.Map Word64 GYTxMetadataValue -> Api.TxMetadata
+makeApiTransactionMetadata = Api.TxMetadata
+
+
+----------------------------------------------------------------------------------------------
+-- Convenience functions for adding messages (comments/memos) following CIP 020 specification.
+----------------------------------------------------------------------------------------------
+
+-- | Adds a single message (comment/memo) as transaction metadata following CIP 020 specification.
 metadataMsg :: Text -> Maybe GYTxMetadata
 metadataMsg msg = metadataMsgs [msg]
 
--- | Adds messages (comments/memos) as transaction metadata following CIP 020 specification.
+-- | Adds multiple messages (comments/memos) as transaction metadata following CIP 020 specification.
 metadataMsgs :: [Text] -> Maybe GYTxMetadata
-metadataMsgs msgs = case repleteMsgs of
-  [] -> Nothing
-  _  -> Just . metadataFromApi . makeApiTransactionMetadataValue $
-        Map.fromList [(674, gyTxMetaMap [(gyTxMetaText "msg", gyTxMetaList $ gyTxMetaText <$> repleteMsgs)])]
+metadataMsgs msgs = case metaValue of
+  Api.TxMetaList [] -> Nothing
+  _                 -> Just . metadataFromApi . makeApiTransactionMetadata $
+                       Map.fromList [(674, gyTxMetaMap [(gyTxMetaText "msg", metaValue)])]
   where
-    repleteMsgs = filter (/= "") msgs
+    metaValue :: GYTxMetadataValue
+    metaValue = foldr metaAppend (Api.TxMetaList []) (gyMetaTextChunks <$> msgs)
+    
+    metaAppend :: GYTxMetadataValue -> GYTxMetadataValue -> GYTxMetadataValue
+    metaAppend (Api.TxMetaList xs) (Api.TxMetaList ys) = Api.TxMetaList (xs ++ ys)
+    metaAppend _ _ = error "Unexpected error applying 'gyMetaTextChunks'"
+

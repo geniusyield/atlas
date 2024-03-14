c5bfc5e930417cdc8f999d572f0fff608e7be3cc
sourabhxyz <swasti@sourabh.xyz>
2024-03-14 18:52:28 +0530

    feat: add `withSetup'` to return for non-unit value. This commit undo the changes caused by formatter

--- src/GeniusYield/Test/Privnet/Setup.hs
+++ src/GeniusYield/Test/Privnet/Setup.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE LambdaCase #-}
-
-{- |
+{-|
 Module      : GeniusYield.Test.Privnet.Setup
 Copyright   : (c) 2023 GYELD GMBH
 License     : Apache 2.0
 Maintainer  : support@geniusyield.co
 Stability   : develop
+
 -}
 module GeniusYield.Test.Privnet.Setup (
     makeSetup,
@@ -14,30 +14,27 @@ module GeniusYield.Test.Privnet.Setup (
     withSetup',
 ) where
 
-import Control.Concurrent (newEmptyMVar, putMVar, takeMVar)
-import Control.Exception (IOException)
-import System.Environment (lookupEnv)
-import System.Exit (exitFailure)
-import System.FilePath ((</>))
+import           Control.Exception              (IOException)
+import           Control.Concurrent             (newEmptyMVar, putMVar, takeMVar)
+import           System.Environment             (lookupEnv)
+import           System.Exit                    (exitFailure)
+import           System.FilePath                ((</>))
 
-import Cardano.Api qualified as Api
+import qualified Cardano.Api                    as Api
 
-import Data.Vector.Fixed qualified as V
+import qualified Data.Vector.Fixed              as V
 
-import GeniusYield.Api.TestTokens qualified as GY.TestTokens
-import GeniusYield.Imports
-import GeniusYield.Providers.Kupo (
-    kupoAwaitTxConfirmed,
-    kupoLookupDatum,
-    kupoQueryUtxo,
-    newKupoApiEnv,
- )
-import GeniusYield.Providers.Node
-import GeniusYield.Test.Privnet.Ctx
-import GeniusYield.Test.Privnet.Paths
-import GeniusYield.Test.Privnet.Utils
-import GeniusYield.TxBuilder
-import GeniusYield.Types
+import qualified GeniusYield.Api.TestTokens     as GY.TestTokens
+import           GeniusYield.Imports
+import           GeniusYield.Providers.Kupo     (kupoAwaitTxConfirmed,
+                                                 kupoLookupDatum, kupoQueryUtxo,
+                                                 newKupoApiEnv)
+import           GeniusYield.Providers.Node
+import           GeniusYield.Test.Privnet.Ctx
+import           GeniusYield.Test.Privnet.Paths
+import           GeniusYield.Test.Privnet.Utils
+import           GeniusYield.TxBuilder
+import           GeniusYield.Types
 
 -- | Era in which privnet runs.
 era :: GYEra
@@ -47,10 +44,9 @@ era = GYBabbage
 -- Setup
 -------------------------------------------------------------------------------
 
-{- | This setup represents a two argument function where first argument is for logging & second represents for continuation, in need of `Ctx`.
-
-Once these two arguments are given to this function, it will give `Ctx` to the continuation, where the logging part (the `ctxLog`) of `Ctx` would be obtained from first argument of this function.
--}
+-- | This setup represents a two argument function where first argument is for logging & second represents for continuation, in need of `Ctx`.
+--
+-- Once these two arguments are given to this function, it will give `Ctx` to the continuation, where the logging part (the `ctxLog`) of `Ctx` would be obtained from first argument of this function.
 newtype Setup = Setup ((String -> IO ()) -> (Ctx -> IO ()) -> IO ())
 
 withSetup :: IO Setup -> (String -> IO ()) -> (Ctx -> IO ()) -> IO ()
@@ -62,26 +58,24 @@ withSetup' :: IO Setup -> (Ctx -> IO a) -> IO a
 withSetup' ioSetup kont = do
     Setup cokont <- ioSetup
     mvar <- newEmptyMVar
-    cokont
-        (const $ return ())
-        ( \ctx -> do
-            res <- kont ctx
-            putMVar mvar res
-            pure ()
-        )
+    cokont (const $ return ()) (\ctx -> do
+        res <- kont ctx
+        putMVar mvar res
+        pure ())
     takeMVar mvar
 
 makeSetup :: IO Setup
 makeSetup = do
     let gyPrivDirVar = "GENIUSYIELD_PRIVNET_DIR"
-        kupoUrlVar = "KUPO_URL"
+        kupoUrlVar   = "KUPO_URL"
     privnetPath' <- lookupEnv gyPrivDirVar
-    kupoUrl <- lookupEnv kupoUrlVar
+    kupoUrl      <- lookupEnv kupoUrlVar
     case privnetPath' of
         Nothing -> communicateError gyPrivDirVar
+
         Just privnetPath -> maybe (communicateError kupoUrlVar) (makeSetup' privnetPath) kupoUrl
-  where
-    communicateError e = return $ Setup $ \info _kont -> info $ e <> " is not set"
+    where
+      communicateError e = return $ Setup $ \info _kont -> info $ e <> " is not set"
 
 debug :: String -> IO ()
 -- FIXME: change me to debug setup code.
@@ -105,24 +99,22 @@ makeSetup' privnetPath kupoUrl = do
     -- Generate user 2 .. 9
     userSkeyAddr <- forM (pathUsers paths) generateUser
     let getUserIdx (i :: Int) = i + 2
-    V.imapM_
-        ( \i (userIskey, userIaddr) -> do
-            debug $ printf "user = %s\n" (show $ getUserIdx i)
-            debug $ printf "user addr = %s\n" userIaddr
-            debug $ printf "user skey = %s\n" (show userIskey)
-            debug $ printf "user vkey = %s\n" (show $ paymentVerificationKey userIskey)
-            debug $ printf "user pkh  = %s\n" (show $ paymentKeyHash $ paymentVerificationKey userIskey)
-        )
-        userSkeyAddr
+    V.imapM_ (
+      \i (userIskey, userIaddr) -> do
+        debug $ printf "user = %s\n" (show $ getUserIdx i)
+        debug $ printf "user addr = %s\n" userIaddr
+        debug $ printf "user skey = %s\n" (show userIskey)
+        debug $ printf "user vkey = %s\n" (show $ paymentVerificationKey userIskey)
+        debug $ printf "user pkh  = %s\n" (show $ paymentKeyHash $ paymentVerificationKey userIskey)
+      ) userSkeyAddr
 
     -- Further down we need local node connection
     let info :: Api.LocalNodeConnectInfo Api.CardanoMode
-        info =
-            Api.LocalNodeConnectInfo
-                { Api.localConsensusModeParams = Api.CardanoModeParams $ Api.EpochSlots 500
-                , Api.localNodeNetworkId = networkIdToApi GYPrivnet
-                , Api.localNodeSocketPath = Api.File $ pathNodeSocket paths
-                }
+        info = Api.LocalNodeConnectInfo
+            { Api.localConsensusModeParams = Api.CardanoModeParams $ Api.EpochSlots 500
+            , Api.localNodeNetworkId       = networkIdToApi GYPrivnet
+            , Api.localNodeSocketPath      = Api.File $ pathNodeSocket paths
+            }
 
     -- ask current slot, so we know local node connection works
     slot <- nodeGetSlotOfCurrentBlock info
@@ -145,39 +137,36 @@ makeSetup' privnetPath kupoUrl = do
     -- context used for tests
     let user' = flip User Nothing
         ctx0 :: Ctx
-        ctx0 =
-            Ctx
-                { ctxEra = era
-                , ctxInfo = info
-                , ctxUserF = User userFskey Nothing userFaddr
-                , ctxUser2 = uncurry user' (V.index userSkeyAddr (Proxy @0))
-                , ctxUser3 = uncurry user' (V.index userSkeyAddr (Proxy @1))
-                , ctxUser4 = uncurry user' (V.index userSkeyAddr (Proxy @2))
-                , ctxUser5 = uncurry user' (V.index userSkeyAddr (Proxy @3))
-                , ctxUser6 = uncurry user' (V.index userSkeyAddr (Proxy @4))
-                , ctxUser7 = uncurry user' (V.index userSkeyAddr (Proxy @5))
-                , ctxUser8 = uncurry user' (V.index userSkeyAddr (Proxy @6))
-                , ctxUser9 = uncurry user' (V.index userSkeyAddr (Proxy @7))
-                , ctxGold = GYLovelace -- temporarily
-                , ctxIron = GYLovelace -- temporarily
-                , ctxLog = noLogging
-                , ctxLookupDatum = localLookupDatum
-                , ctxAwaitTxConfirmed = localAwaitTxConfirmed
-                , ctxQueryUtxos = localQueryUtxo
-                , ctxGetParams = localGetParams
-                }
+        ctx0 = Ctx
+            { ctxEra              = era
+            , ctxInfo             = info
+            , ctxUserF            = User userFskey Nothing userFaddr
+            , ctxUser2            = uncurry user' (V.index userSkeyAddr (Proxy @0))
+            , ctxUser3            = uncurry user' (V.index userSkeyAddr (Proxy @1))
+            , ctxUser4            = uncurry user' (V.index userSkeyAddr (Proxy @2))
+            , ctxUser5            = uncurry user' (V.index userSkeyAddr (Proxy @3))
+            , ctxUser6            = uncurry user' (V.index userSkeyAddr (Proxy @4))
+            , ctxUser7            = uncurry user' (V.index userSkeyAddr (Proxy @5))
+            , ctxUser8            = uncurry user' (V.index userSkeyAddr (Proxy @6))
+            , ctxUser9            = uncurry user' (V.index userSkeyAddr (Proxy @7))
+            , ctxGold             = GYLovelace -- temporarily
+            , ctxIron             = GYLovelace -- temporarily
+            , ctxLog              = noLogging
+            , ctxLookupDatum      = localLookupDatum
+            , ctxAwaitTxConfirmed = localAwaitTxConfirmed
+            , ctxQueryUtxos       = localQueryUtxo
+            , ctxGetParams        = localGetParams
+            }
 
-    userBalances <-
-        V.imapM
-            ( \i (_, userIaddr) -> do
-                userIbalance <- ctxRunC ctx0 (ctxUserF ctx0) $ queryBalance userIaddr
-                when (isEmptyValue userIbalance) $ do
-                    debug $ printf "User %s balance is empty, giving some ada\n" (show $ getUserIdx i)
-                    giveAda ctx0 userIaddr
-                    when (i == 0) (giveAda ctx0 userFaddr) -- we also give ada to itself to create some small utxos
-                ctxRunC ctx0 (ctxUserF ctx0) $ queryBalance userIaddr
-            )
-            userSkeyAddr
+    userBalances <- V.imapM
+      (\i (_, userIaddr) -> do
+        userIbalance <- ctxRunC ctx0 (ctxUserF ctx0) $ queryBalance userIaddr
+        when (isEmptyValue userIbalance) $ do
+            debug $ printf "User %s balance is empty, giving some ada\n" (show $ getUserIdx i)
+            giveAda ctx0 userIaddr
+            when (i == 0) (giveAda ctx0 userFaddr) -- we also give ada to itself to create some small utxos
+        ctxRunC ctx0 (ctxUserF ctx0) $ queryBalance userIaddr
+      ) userSkeyAddr
 
     -- mint test tokens
     goldAC <- mintTestTokens paths ctx0 "GOLD"
@@ -187,39 +176,35 @@ makeSetup' privnetPath kupoUrl = do
     debug $ printf "iron = %s\n" ironAC
 
     let ctx :: Ctx
-        ctx =
-            ctx0
-                { ctxGold = goldAC
-                , ctxIron = ironAC
-                }
+        ctx = ctx0
+            { ctxGold = goldAC
+            , ctxIron = ironAC
+            }
 
     -- distribute tokens
     V.imapM_
-        ( \i userIbalance -> do
-            when (isEmptyValue $ snd $ valueSplitAda userIbalance) $ do
-                debug $ printf "User%s has no tokens, giving some\n" (show $ getUserIdx i)
-                giveTokens ctx (snd $ userSkeyAddr V.! i)
-        )
-        userBalances
+      (\i userIbalance -> do
+        when (isEmptyValue $ snd $ valueSplitAda userIbalance) $ do
+          debug $ printf "User%s has no tokens, giving some\n" (show $ getUserIdx i)
+          giveTokens ctx (snd $ userSkeyAddr V.! i)
+      ) userBalances
 
-    return $
-        Setup
-            (\putLog kont -> kont $ ctx{ctxLog = simpleConsoleLogging putLog})
+    return $ Setup
+        (\putLog kont -> kont $ ctx { ctxLog = simpleConsoleLogging putLog })
 
 -------------------------------------------------------------------------------
 -- Generating users
 -------------------------------------------------------------------------------
 
 generateUser :: UserPaths -> IO (GYPaymentSigningKey, GYAddress)
-generateUser UserPaths{..} =
+generateUser UserPaths {..} =
     existing `catchIOException` const new
   where
     existing = do
-        skey <-
-            Api.readFileTextEnvelope (Api.AsSigningKey Api.AsPaymentKey) (Api.File pathUserSKey)
-                >>= \case
-                    Right skey -> return $ paymentSigningKeyFromApi skey
-                    Left err -> throwIO $ userError $ show err
+        skey <- Api.readFileTextEnvelope (Api.AsSigningKey Api.AsPaymentKey) (Api.File pathUserSKey) >>=
+          \case
+          Right skey -> return $ paymentSigningKeyFromApi skey
+          Left err   -> throwIO $ userError $ show err
 
         addr <- addressFromBech32 <$> urlPieceFromFile pathUserAddr
         return (skey, addr)
@@ -229,35 +214,33 @@ generateUser UserPaths{..} =
         skey <- runAddressKeyGen pathUserSKey
 
         -- construct address (no stake)
-        let vkey = Api.getVerificationKey skey
+        let vkey     = Api.getVerificationKey skey
             vkeyHash = Api.verificationKeyHash vkey
 
         let addr :: GYAddress
-            addr =
-                addressFromApi' $
-                    Api.AddressInEra
-                        (Api.ShelleyAddressInEra Api.ShelleyBasedEraBabbage)
-                        (Api.makeShelleyAddress network (Api.PaymentCredentialByKey vkeyHash) stake)
+            addr = addressFromApi' $ Api.AddressInEra
+                (Api.ShelleyAddressInEra Api.ShelleyBasedEraBabbage)
+                (Api.makeShelleyAddress network (Api.PaymentCredentialByKey vkeyHash) stake)
 
         urlPieceToFile pathUserAddr (addressToBech32 addr)
 
         return (paymentSigningKeyFromApi skey, addr)
 
     network = networkIdToApi GYPrivnet
-    stake = Api.NoStakeAddress
+    stake   = Api.NoStakeAddress
 
-runAddressKeyGen ::
-    -- | signing key path
-    FilePath ->
-    IO (Api.SigningKey Api.PaymentKey)
+runAddressKeyGen
+    :: FilePath  -- ^ signing key path
+    -> IO (Api.SigningKey Api.PaymentKey)
 runAddressKeyGen skeyPath = do
-    skey <- Api.generateSigningKey Api.AsPaymentKey
-    res <- Api.writeFileTextEnvelope (Api.File skeyPath) (Just skeyDesc) skey
-    case res of
-        Right () -> return skey
-        Left err -> do
-            printf "Failed to write %s: %s\n" skeyPath (show err)
-            exitFailure
+      skey <- Api.generateSigningKey Api.AsPaymentKey
+      res <- Api.writeFileTextEnvelope (Api.File skeyPath) (Just skeyDesc) skey
+      case res of
+          Right () -> return skey
+          Left err -> do
+              printf "Failed to write %s: %s\n" skeyPath (show err)
+              exitFailure
+
   where
     skeyDesc :: Api.TextEnvelopeDescr
     skeyDesc = "Payment Signing Key"
@@ -268,22 +251,15 @@ runAddressKeyGen skeyPath = do
 
 giveAda :: Ctx -> GYAddress -> IO ()
 giveAda ctx addr = do
-    txBody <-
-        ctxRunI ctx (ctxUserF ctx) $
-            return $
-                mconcat $
-                    replicate 5 $
-                        mustHaveOutput $
-                            mkGYTxOutNoDatum addr (valueFromLovelace 1_000_000_000)
+    txBody <- ctxRunI ctx (ctxUserF ctx) $ return $ mconcat $ replicate 5 $
+        mustHaveOutput $ mkGYTxOutNoDatum addr (valueFromLovelace 1_000_000_000)
     void $ submitTx ctx (ctxUserF ctx) txBody
 
 giveTokens :: Ctx -> GYAddress -> IO ()
 giveTokens ctx addr = do
-    txBody <-
-        ctxRunI ctx (ctxUserF ctx) $
-            return $
-                mustHaveOutput (mkGYTxOutNoDatum addr (valueSingleton (ctxGold ctx) 1_000_000))
-                    <> mustHaveOutput (mkGYTxOutNoDatum addr (valueSingleton (ctxIron ctx) 1_000_000))
+    txBody <- ctxRunI ctx (ctxUserF ctx) $ return $
+        mustHaveOutput (mkGYTxOutNoDatum addr (valueSingleton (ctxGold ctx) 1_000_000)) <>
+        mustHaveOutput (mkGYTxOutNoDatum addr (valueSingleton (ctxIron ctx) 1_000_000))
     void $ submitTx ctx (ctxUserF ctx) txBody
 
 -------------------------------------------------------------------------------
@@ -291,7 +267,7 @@ giveTokens ctx addr = do
 -------------------------------------------------------------------------------
 
 mintTestTokens :: Paths -> Ctx -> String -> IO GYAssetClass
-mintTestTokens Paths{pathGeniusYield} ctx tn' = do
+mintTestTokens Paths {pathGeniusYield} ctx tn' = do
     debug $ printf "Minting token %s\n" (show tn)
     existing `catchIOException` const new
   where
@@ -306,9 +282,8 @@ mintTestTokens Paths{pathGeniusYield} ctx tn' = do
 
     new :: IO GYAssetClass
     new = do
-        (ac, txBody) <-
-            ctxRunF ctx (ctxUserF ctx) $
-                GY.TestTokens.mintTestTokens tn 10_000_000
+        (ac, txBody) <- ctxRunF ctx (ctxUserF ctx) $
+            GY.TestTokens.mintTestTokens tn 10_000_000
         void $ submitTx ctx (ctxUserF ctx) txBody
 
         urlPieceToFile path ac

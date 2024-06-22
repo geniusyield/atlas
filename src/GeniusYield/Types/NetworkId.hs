1130a8e748ce8d74f020ca351d2978b6ece3a386
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2024-06-21 23:38:10 -0600

    Dynamic `GYPrivnet`

--- src/GeniusYield/Types/NetworkId.hs
+++ src/GeniusYield/Types/NetworkId.hs
@@ -8,7 +8,6 @@ Stability   : develop
 -}
 module GeniusYield.Types.NetworkId
     ( GYNetworkId (..)
-    , networkIdFromApi
     , networkIdToApi
     , networkIdToLedger
     , networkIdToEpochSlots
@@ -19,6 +18,7 @@ import qualified Cardano.Api              as Api
 import qualified Cardano.Ledger.BaseTypes as Ledger
 import qualified Data.Aeson.Types         as Aeson
 import qualified Data.Text                as T
+import           Data.Word               (Word32, Word64)
 
 import           GeniusYield.Types.Era
 
@@ -29,27 +29,24 @@ import           GeniusYield.Types.Era
 -- >>> import qualified Data.ByteString.Lazy.Char8 as LBS8
 
 data GYNetworkId
-    = GYMainnet        -- ^ cardano mainnet
-    | GYTestnetPreprod -- ^ cardano preprod testnet
-    | GYTestnetPreview -- ^ cardano preview testnet
-    | GYTestnetLegacy  -- ^ cardano legacy testnet
-    | GYPrivnet        -- ^ local private network
+    = GYMainnet         -- ^ cardano mainnet
+    | GYTestnetPreprod  -- ^ cardano preprod testnet
+    | GYTestnetPreview  -- ^ cardano preview testnet
+    | GYTestnetLegacy   -- ^ cardano legacy testnet
+    | GYPrivnet
+        { gyNetworkMagic :: Word32
+        , gyNetworkEpochSlots :: Word64
+        , gyNetworkEra :: GYEra
+        }
+      -- ^ local private network
     deriving (Show, Read, Eq, Ord)
 
-networkIdFromApi :: Api.NetworkId -> Maybe GYNetworkId
-networkIdFromApi Api.Mainnet                                 = Just GYMainnet
-networkIdFromApi (Api.Testnet (Api.NetworkMagic 1))          = Just GYTestnetPreprod
-networkIdFromApi (Api.Testnet (Api.NetworkMagic 2))          = Just GYTestnetPreview
-networkIdFromApi (Api.Testnet (Api.NetworkMagic 1097911063)) = Just GYTestnetLegacy
-networkIdFromApi (Api.Testnet (Api.NetworkMagic 42))         = Just GYPrivnet
-networkIdFromApi (Api.Testnet (Api.NetworkMagic _))          = Nothing
-
 networkIdToApi :: GYNetworkId -> Api.NetworkId
-networkIdToApi GYMainnet        = Api.Mainnet
-networkIdToApi GYTestnetPreprod = Api.Testnet $ Api.NetworkMagic 1
-networkIdToApi GYTestnetPreview = Api.Testnet $ Api.NetworkMagic 2
-networkIdToApi GYTestnetLegacy  = Api.Testnet $ Api.NetworkMagic 1097911063
-networkIdToApi GYPrivnet        = Api.Testnet $ Api.NetworkMagic 42
+networkIdToApi GYMainnet                 = Api.Mainnet
+networkIdToApi GYTestnetPreprod          = Api.Testnet $ Api.NetworkMagic 1
+networkIdToApi GYTestnetPreview          = Api.Testnet $ Api.NetworkMagic 2
+networkIdToApi GYTestnetLegacy           = Api.Testnet $ Api.NetworkMagic 1097911063
+networkIdToApi GYPrivnet{gyNetworkMagic} = Api.Testnet $ Api.NetworkMagic gyNetworkMagic
 
 networkIdToLedger :: GYNetworkId -> Ledger.Network
 networkIdToLedger nid = case networkIdToApi nid of
@@ -57,19 +54,19 @@ networkIdToLedger nid = case networkIdToApi nid of
     Api.Testnet _magic -> Ledger.Testnet
 
 networkIdToEpochSlots :: GYNetworkId -> Api.EpochSlots
-networkIdToEpochSlots GYPrivnet        = Api.EpochSlots 500
-networkIdToEpochSlots GYMainnet        = Api.EpochSlots 432000
-networkIdToEpochSlots GYTestnetPreprod = Api.EpochSlots 432000
-networkIdToEpochSlots GYTestnetPreview = Api.EpochSlots 86400
-networkIdToEpochSlots GYTestnetLegacy  = Api.EpochSlots 432000
+networkIdToEpochSlots GYPrivnet{gyNetworkEpochSlots} = Api.EpochSlots gyNetworkEpochSlots
+networkIdToEpochSlots GYMainnet                      = Api.EpochSlots 432000
+networkIdToEpochSlots GYTestnetPreprod               = Api.EpochSlots 432000
+networkIdToEpochSlots GYTestnetPreview               = Api.EpochSlots 86400
+networkIdToEpochSlots GYTestnetLegacy                = Api.EpochSlots 432000
 
 -- This needs to be updated whenever a hardfork happens.
 networkIdToEra :: GYNetworkId -> GYEra
-networkIdToEra GYPrivnet        = GYBabbage
-networkIdToEra GYMainnet        = GYBabbage
-networkIdToEra GYTestnetPreprod = GYBabbage
-networkIdToEra GYTestnetPreview = GYBabbage
-networkIdToEra GYTestnetLegacy  = GYBabbage
+networkIdToEra GYPrivnet{gyNetworkEra} = gyNetworkEra
+networkIdToEra GYMainnet               = GYBabbage
+networkIdToEra GYTestnetPreprod        = GYBabbage
+networkIdToEra GYTestnetPreview        = GYBabbage
+networkIdToEra GYTestnetLegacy         = GYBabbage
 
 -------------------------------------------------------------------------------
 -- aeson
@@ -89,13 +86,13 @@ instance Aeson.ToJSON GYNetworkId where
     toJSON GYTestnetPreprod = Aeson.toJSON ("testnet-preprod" :: T.Text)
     toJSON GYTestnetPreview = Aeson.toJSON ("testnet-preview" :: T.Text)
     toJSON GYTestnetLegacy  = Aeson.toJSON ("testnet" :: T.Text)
-    toJSON GYPrivnet        = Aeson.toJSON ("privnet" :: T.Text)
+    toJSON GYPrivnet{}      = Aeson.toJSON ("privnet" :: T.Text)
 
     toEncoding GYMainnet        = Aeson.toEncoding ("mainnet" :: T.Text)
     toEncoding GYTestnetPreprod = Aeson.toEncoding ("testnet-preprod" :: T.Text)
     toEncoding GYTestnetPreview = Aeson.toEncoding ("testnet-preview" :: T.Text)
     toEncoding GYTestnetLegacy  = Aeson.toEncoding ("testnet" :: T.Text)
-    toEncoding GYPrivnet        = Aeson.toEncoding ("privnet" :: T.Text)
+    toEncoding GYPrivnet{}      = Aeson.toEncoding ("privnet" :: T.Text)
 
 -- |
 --
@@ -109,5 +106,4 @@ instance Aeson.FromJSON GYNetworkId where
     parseJSON "testnet-preview" = pure GYTestnetPreview
     parseJSON "preview"         = pure GYTestnetPreview
     parseJSON "testnet"         = pure GYTestnetLegacy
-    parseJSON "privnet"         = pure GYPrivnet
-    parseJSON _                 = fail "Expected mainnet, testnet-preprod, preprod, testnet-preview, preview, testnet or privnet"
+    parseJSON _                 = fail "Expected mainnet, testnet-preprod, preprod, testnet-preview, preview or testnet"

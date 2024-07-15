31fa45ccb3be119f41f2cd773496b76d824165ed
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2024-07-15 16:03:44 -0600

    More utilities for `User`

--- src/GeniusYield/TxBuilder/User.hs
+++ src/GeniusYield/TxBuilder/User.hs
@@ -13,12 +13,14 @@ module GeniusYield.TxBuilder.User (
     userStakeSKey',
     userStakeVKey,
     userCollateralDumb,
+    userAddresses',
     userAddr,
 ) where
 
 import           Data.List.NonEmpty               (NonEmpty)
 import qualified Data.List.NonEmpty               as NE
 
+import           GeniusYield.Imports
 import           GeniusYield.Types.Address        (GYAddress)
 import           GeniusYield.Types.Key
 import           GeniusYield.Types.Key.Class      (ToShelleyWitnessSigningKey (toShelleyWitnessSigningKey))
@@ -33,7 +35,7 @@ data UserCollateral = UserCollateral
     , userCollateralCheck :: Bool
     -- ^ If `False` then the given `GYTxOutRef` will be used and reserved as collateral.
     -- If `True`, then collateral will only be used and reserved, if value in the given UTxO is exactly 5 ada.
-    }
+    } deriving stock (Eq, Show)
 
 -- | Note: When signing using 'ToShelleyWitnessSigningKey' instance, it only uses the payment signing key.
 data User = User
@@ -42,7 +44,10 @@ data User = User
     , userAddresses       :: !(NonEmpty GYAddress)
     , userChangeAddress   :: !GYAddress
     , userCollateral      :: Maybe UserCollateral
-    }
+    } deriving stock (Eq, Show)
+
+instance Ord User where
+  compare = compare `on` userChangeAddress
 
 -- | This only takes the payment signing key, not the stake key.
 instance ToShelleyWitnessSigningKey User where
@@ -70,6 +75,9 @@ userStakePkh = fmap (stakeKeyHash . stakeVerificationKey) . userStakeSKey
 userCollateralDumb :: User -> Maybe (GYTxOutRef, Bool)
 userCollateralDumb User{userCollateral} =
     (\UserCollateral {userCollateralRef, userCollateralCheck} -> (userCollateralRef, userCollateralCheck)) <$> userCollateral
+
+userAddresses' :: User -> [GYAddress]
+userAddresses' = NE.toList . userAddresses
 
 pattern User' :: GYPaymentSigningKey -> Maybe GYStakeSigningKey -> GYAddress -> User
 pattern User' { userPaymentSKey', userStakeSKey', userAddr } <- User

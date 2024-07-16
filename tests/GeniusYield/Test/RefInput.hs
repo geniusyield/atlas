b2690eb8436b64b665dcef954158047fb9c02df2
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2024-07-15 22:27:28 -0600

    Unified testing interface demonstrated by RefInput

--- tests/GeniusYield/Test/RefInput.hs
+++ tests/GeniusYield/Test/RefInput.hs
@@ -17,11 +17,13 @@ import           Test.Tasty                                           (TestTree,
                                                                        testGroup)
 
 import           GeniusYield.Imports
-import           GeniusYield.Test.GYTxBody                            (mockTxId)
+import           GeniusYield.HTTP.Errors
+import           GeniusYield.Test.Clb
+import           GeniusYield.Test.FeeTracker
 import           GeniusYield.Test.OnChain.GuessRefInputDatum.Compiled
 import           GeniusYield.Test.Utils
+import           GeniusYield.Transaction
 import           GeniusYield.TxBuilder
-import           GeniusYield.Test.Clb
 import           GeniusYield.Types
 
 gyGuessRefInputDatumValidator :: GYValidator 'PlutusV2
@@ -31,10 +33,12 @@ refInputTests :: TestTree
 refInputTests = testGroup "Reference Input"
     [ mkTestFor "Inlined datum" $ refInputTrace True 5 5
     , mkTestFor "Inlined datum - Wrong guess" $ mustFail . refInputTrace True 5 4
-    , mkTestFor "Reference input must not be consumed" tryRefInputConsume
+    , mkTestFor "Reference input must not be consumed" $
+        mustFailWith (\case { GYBuildTxException (GYBuildTxBalancingError (GYBalancingErrorInsufficientFunds _)) -> True; _ -> False })
+        . tryRefInputConsume
     ]
 
-guessRefInputRun :: GYTxOutRef -> GYTxOutRef -> Integer -> GYTxMonadClb ()
+guessRefInputRun :: GYTxMonad m => GYTxOutRef -> GYTxOutRef -> Integer -> m ()
 guessRefInputRun refInputORef consumeRef guess = do
   let redeemer = Guess guess
       skeleton :: GYTxSkeleton 'PlutusV2 =
@@ -46,49 +50,43 @@ guessRefInputRun refInputORef consumeRef guess = do
               (redeemerFromPlutusData redeemer)
           } <>
         mustHaveRefInput refInputORef
-  void $ sendSkeleton skeleton
+  buildTxBody skeleton >>= signAndSubmitConfirmed_
 
-refInputTrace :: Bool -> Integer -> Integer -> Wallets -> GYTxMonadClb ()
+refInputTrace :: GYTxGameMonad m => Bool -> Integer -> Integer -> Wallets -> m ()
 refInputTrace toInline actual guess Wallets{..} = do
   let myGuess :: Integer = guess
       outValue :: GYValue = valueFromLovelace 20_000_000
-  mMOref <- runWallet w1 $ addRefInput toInline (walletAddress w9) (datumFromPlutusData (RefInputDatum actual))
+  mMOref <- asUser w1 $ addRefInput toInline (userAddr w9) (datumFromPlutusData (RefInputDatum actual))
   case mMOref of
-    Nothing -> fail "Unable to create utxo to reference"
-    Just Nothing -> fail "Couldn't find index for reference utxo in outputs"
-    Just (Just refInputORef) ->
-      void $ runWallet w1 $ withWalletBalancesCheckSimple [w1 := valueFromLovelace 0] $ do
-        liftClb $ logInfoS $ printf "Reference input ORef %s" refInputORef
+    Nothing -> throwAppError $ someBackendError "Couldn't find index for reference utxo in outputs"
+    Just refInputORef ->
+      withWalletBalancesCheckSimple [w1 := valueFromLovelace 0] . asUser w1 $ do
+        gyLogInfo' "" $ printf "Reference input ORef %s" refInputORef
         addr <- scriptAddress gyGuessRefInputDatumValidator
-        (tx, txId) <- sendSkeleton' (mustHaveOutput $ mkGYTxOut addr outValue (datumFromPlutusData ())) []
+        txBody <- buildTxBody . mustHaveOutput $ mkGYTxOut addr outValue (datumFromPlutusData ())
+        tx <- signTxBody txBody
+        txId <- submitTxConfirmed tx
         let mOrefIndices = findLockedUtxosInBody addr tx
-        orefIndices <- maybe (fail "Unable to get GYAddress from some Plutus.Address in txBody") return mOrefIndices
+        orefIndices <- maybe (throwAppError . someBackendError $ "Unable to get GYAddress from some Plutus.Address in txBody") return mOrefIndices
         oref        <- case fmap (txOutRefFromApiTxIdIx (txIdToApi txId) . wordToApiIx) orefIndices of
           [oref']        -> return oref'
-          _non_singleton -> fail "expected exactly one reference"
-        liftClb $ logInfoS $ printf "Locked ORef %s" oref
+          _non_singleton -> throwAppError . someBackendError $ "expected exactly one reference"
+        gyLogInfo' "" $ printf "Locked ORef %s" oref
         guessRefInputRun refInputORef oref myGuess
 
-tryRefInputConsume :: Wallets -> GYTxMonadClb ()
+tryRefInputConsume :: GYTxGameMonad m => Wallets -> m ()
 tryRefInputConsume Wallets{..} = do
   -- Approach: Create a new output with 60% of total ada. Mark this UTxO as reference input and try sending this same 60%, or any amount greater than 40% of this original balance. Since coin balancer can't consume this UTxO, it won't be able to build for it.
-  void $ runWallet w1 $ do
-    walletBalance <- balance w1
+  void $ asUser w1 $ do
+    walletBalance <- queryBalance $ userAddr w1
     let walletLovelaceBalance = fst $ valueSplitAda walletBalance
         lovelaceToSend = (walletLovelaceBalance `div` 10) * 6  -- send 60% of total ada
         lovelaceToSendValue = valueFromLovelace lovelaceToSend
-    (tx, txId) <- sendSkeleton' (mustHaveOutput $ mkGYTxOutNoDatum (walletAddress w1) lovelaceToSendValue) []
-    bodyUtxos <- utxosInBody tx txId
-    let bodyUtxos' = catMaybes bodyUtxos
-    unless (length bodyUtxos == length bodyUtxos') $ fail $ printf "Shouldn't happen: Not all UTxOs reflected, originally %s but got %s and they are %s" (show $ length bodyUtxos) (show $ length bodyUtxos') (show bodyUtxos')
-    desiredOutputRef <- case utxoRef <$> find (\GYUTxO{ utxoValue } -> utxoValue == lovelaceToSendValue) bodyUtxos' of
-          Nothing  -> fail "Shouldn't happen: Couldn't find the desired UTxO"
+    txBody <- buildTxBody . mustHaveOutput $ mkGYTxOutNoDatum (userAddr w1) lovelaceToSendValue
+    signAndSubmitConfirmed_ txBody
+    let bodyUtxos = utxosToList $ txBodyUTxOs txBody
+    desiredOutputRef <- case utxoRef <$> find (\GYUTxO{ utxoValue } -> utxoValue == lovelaceToSendValue) bodyUtxos of
+          Nothing  -> throwAppError . someBackendError $ "Shouldn't happen: Couldn't find the desired UTxO"
           Just ref -> pure ref
-    sendSkeleton (mustHaveRefInput @'PlutusV2 desiredOutputRef <> mustHaveOutput  (mkGYTxOutNoDatum (walletAddress w1) lovelaceToSendValue))
-      `catchError` (
-        \case
-          GYApplicationException e -> do
-            liftClb $ logInfoS $ printf "Successfully caught expected exception %s" (show e)
-            pure mockTxId
-          e -> fail $ printf "Unexpected exception %s" (show e)
-        )
+    buildTxBody (mustHaveRefInput @'PlutusV2 desiredOutputRef <> mustHaveOutput  (mkGYTxOutNoDatum (userAddr w1) lovelaceToSendValue))
+      >>= signAndSubmitConfirmed_

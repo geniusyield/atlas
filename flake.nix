a7eb25a9823dd12b3126ba80eb8cb2d9adec02a8
sourabhxyz <swasti@sourabh.xyz>
2024-08-29 15:53:08 +0530

    feat(#326): update nix

--- flake.nix
+++ flake.nix
@@ -4,39 +4,42 @@
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.CHaP = {
-      url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
-      flake = false;
-    };
+    url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+    flake = false;
+  };
   outputs = { self, nixpkgs, flake-utils, haskellNix, CHaP }:
     let
       supportedSystems = [
-        "x86_64-linux"	"x86_64-darwin"	"aarch64-darwin"
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
     in
-      flake-utils.lib.eachSystem supportedSystems (system:
+    flake-utils.lib.eachSystem supportedSystems (system:
       let
         overlay = final: prev: {
           haskell-nix = prev.haskell-nix // {
             extraPkgconfigMappings = prev.haskell-nix.extraPkgconfigMappings // {
-                # String pkgconfig-depends names are mapped to lists of Nixpkgs
-                # package names
-                "libblst" = [ "blst" ];
+              # String pkgconfig-depends names are mapped to lists of Nixpkgs
+              # package names
+              "libblst" = [ "blst" ];
             };
           };
         };
 
-        overlays = [ haskellNix.overlay
+        overlays = [
+          haskellNix.overlay
           (final: prev: {
             hixProject =
               final.haskell-nix.project' {
                 src = ./.;
-                compiler-nix-name = "ghc964";
+                compiler-nix-name = "ghc965";
                 # This is used by `nix develop .` to open a shell for use with
                 # `cabal`, `hlint` and `haskell-language-server`
                 shell.tools = {
-                  cabal = {}  ;
-                  hlint = {};
-                  haskell-language-server = {};
+                  cabal = { };
+                  hlint = { };
+                  haskell-language-server = { };
                 };
                 # Non-Haskell shell tools go here
                 shell.buildInputs = with pkgs; [
@@ -48,8 +51,9 @@
           overlay
         ];
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.hixProject.flake {};
-      in flake // {
+        flake = pkgs.hixProject.flake { };
+      in
+      flake // {
         legacyPackages = pkgs;
       });
 }

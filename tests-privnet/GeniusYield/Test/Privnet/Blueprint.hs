2993139eb1c32d4e7256e43603e346a17f6b77de
sourabhxyz <swasti@sourabh.xyz>
2024-10-07 13:09:50 +0530

    feat(#355): refactor blueprint test to utilise unified testing framework

--- tests-privnet/GeniusYield/Test/Privnet/Blueprint.hs
+++ tests-privnet/GeniusYield/Test/Privnet/Blueprint.hs
@@ -7,16 +7,17 @@ module GeniusYield.Test.Privnet.Blueprint (
   blueprintTests,
 ) where
 
+import Control.Monad (void)
+import Control.Monad.Except (handleError)
 import Data.ByteString (ByteString)
 import Data.ByteString.Base16 qualified as BS16
-import GeniusYield.Test.Privnet.Asserts (assertThrown, isTxBodyErrorAutoBalance)
-import GeniusYield.Test.Privnet.Ctx
+import GeniusYield.Test.Privnet.Asserts (isTxBodyErrorAutoBalance)
 import GeniusYield.Test.Privnet.Setup
+import GeniusYield.Test.Utils (TestInfo (testWallets), Wallets (..))
 import GeniusYield.TxBuilder
 import GeniusYield.Types
 import PlutusTx.Builtins qualified as PlutusTx
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (testCaseSteps)
 
 $(makeBPTypes "tests/aiken/bar/plutus.json")
 
@@ -26,25 +27,28 @@ blueprintTests :: Setup -> TestTree
 blueprintTests setup =
   testGroup
     "blueprint"
-    [ testCaseSteps "e2e blueprint validator test" $ \info -> withSetup info setup $ \ctx -> do
-        let user = ctxUserF ctx
-            val = scriptFromBPSerialisedScript $ applyParamsToBPValidator_baz_baz_spend BPBool1True (BPbaz_ParamConstr0ParamConstr 23 (g "ff")) 10 (g "ddee")
+    [ mkPrivnetTestFor' "e2e blueprint validator test" GYInfo setup $ \(testWallets -> Wallets {w1 = user}) -> do
+        let val = scriptFromBPSerialisedScript $ applyParamsToBPValidator_baz_baz_spend BPBool1True (BPbaz_ParamConstr0ParamConstr 23 (g "ff")) 10 (g "ddee")
             dat = datumFromPlutusData $ BPbaz_MyDatum0DatumA 10 (g "aabbccdd")
-        oref <- ctxRun ctx user $ do
+        oref <- asUser user $ do
           valAddr <- scriptAddress val
           txBody <- buildTxBody $ mustHaveOutput @'PlutusV3 (GYTxOut {gyTxOutValue = valueFromLovelace 10_000_000, gyTxOutRefS = Nothing, gyTxOutDatum = Just (dat, GYTxOutUseInlineDatum), gyTxOutAddress = valAddr})
           tid <- signAndSubmitConfirmed txBody
           pure $ txOutRefFromTuple (tid, 0)
         let satRedeemer = redeemerFromPlutusData $ BPbaz_MyRedeemer0MyRedeemer 5 (g "aabbccddee")
             unsatRedeemer = redeemerFromPlutusData $ BPbaz_MyRedeemer0MyRedeemer 6 (g "aabbccddee")
-        assertThrown isTxBodyErrorAutoBalance $ ctxRun ctx user $ do
-          buildTxBody $ mustHaveInput @'PlutusV3 $ GYTxIn oref (GYTxInWitnessScript (GYInScript val) dat unsatRedeemer)
-        info "Successfully failed to consume from blueprint script for unsatisfying redeemer"
-        tid <- ctxRun ctx user $ do
+        handleError
+          (\e -> if isTxBodyErrorAutoBalance e then pure () else throwError e)
+          $ asUser user
+          $ do
+            void $ buildTxBody $ mustHaveInput @'PlutusV3 $ GYTxIn oref (GYTxInWitnessScript (GYInScript val) dat unsatRedeemer)
+        lg "Successfully failed to consume from blueprint script for unsatisfying redeemer"
+        tid <- asUser user $ do
           txBody <- buildTxBody $ mustHaveInput @'PlutusV3 $ GYTxIn oref (GYTxInWitnessScript (GYInScript val) dat satRedeemer)
           signAndSubmitConfirmed txBody
-        info $ "Successfully consumed from blueprint script, with tx id: " <> show tid
+        lg $ "Successfully consumed from blueprint script, with tx id: " <> show tid
     ]
  where
   g :: ByteString -> PlutusTx.BuiltinByteString
   g = PlutusTx.toBuiltin . BS16.decodeLenient
+  lg = gyLogInfo' "blueprint"
